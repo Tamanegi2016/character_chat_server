8f9a8e40edc98a92937846cf472b9a3d39a6f3bb
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-07-16 10:50:50 +0900

    fix bug

--- db/migrate/20160716014634_create_users.rb
+++ db/migrate/20160716014634_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.varchar :name
-      t.varchar :profile
+      t.string :name
+      t.string :profile_img
 
       t.timestamps
     end
--- db/migrate/20160716015000_create_talks.rb
+++ db/migrate/20160716015000_create_talks.rb
@@ -1,8 +1,8 @@
 class CreateTalks < ActiveRecord::Migration[5.0]
   def change
     create_table :talks do |t|
-      t.int :user_id
-      t.varchar :message
+      t.integer :user_id
+      t.string :message
 
       t.timestamps
     end
