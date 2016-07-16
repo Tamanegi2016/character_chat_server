b3ced17007b072898747ecbfbeea303e7ed24c65
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-07-16 18:55:48 +0900

    merge

--- db/migrate/20160716052253_create_no_session_talks.rb
+++ db/migrate/20160716052253_create_no_session_talks.rb
@@ -1,8 +1,8 @@
 class CreateNoSessionTalks < ActiveRecord::Migration[5.0]
   def change
     create_table :no_session_talks do |t|
-
       t.timestamps
+      #t.references :no_session_user, index: true, foreign_key: true
     end
   end
 end
