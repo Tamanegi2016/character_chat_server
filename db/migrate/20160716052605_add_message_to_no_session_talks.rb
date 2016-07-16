65cdfc5db5e2f37fc389b2c724b510d75f67a44f
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-07-16 14:55:48 +0900

    fix db

--- db/migrate/20160716052605_add_message_to_no_session_talks.rb
+++ db/migrate/20160716052605_add_message_to_no_session_talks.rb
@@ -1,6 +1,6 @@
 class AddMessageToNoSessionTalks < ActiveRecord::Migration[5.0]
   def change
     add_column :no_session_talks, :message, :string
-    add_column :no_session_talks, :user_id, :integer
+    add_reference :no_session_talks, :users
   end
 end
