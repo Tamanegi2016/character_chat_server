5d8830d47b2ea0535a1bd9c47885cd725ac5ab3b
Kento Watanabe <k3nt0.watanabe@gmail.com>
2016-07-17 14:06:32 +0900

    references

--- db/migrate/20160716064823_create_friends.rb
+++ db/migrate/20160716064823_create_friends.rb
@@ -1,9 +1,5 @@
 class CreateFriends < ActiveRecord::Migration[5.0]
-  def change
-    create_table :friends do |t|
-      t.references :to_user, class_name: "CreateNoSessionUsers", foreign_key: :no_session_users_id
-      t.references :from_user, class_name: "CreateNoSessionUsers", foreign_key: :no_session_users_id
+      belongs_to :to_user, class_name: "CreateNoSessionUsers", foreign_key: :no_session_users_id
+      belongs_to :from_user, class_name: "CreateNoSessionUsers", foreign_key: :no_session_users_id
       t.timestamp
-    end
-  end
 end
