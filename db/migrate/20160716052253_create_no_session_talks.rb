6794ecfd0887821a4556795eb1c2d405137048cc
Kento Watanabe <k3nt0.watanabe@gmail.com>
2016-07-17 15:42:02 +0900

    hoge

--- db/migrate/20160716052253_create_no_session_talks.rb
+++ db/migrate/20160716052253_create_no_session_talks.rb
@@ -1,7 +1,7 @@
 class CreateNoSessionTalks < ActiveRecord::Migration[5.0]
   def change
     create_table :no_session_talks do |t|
-      t.references :no_session_user_id
+      t.references :no_session_user
       t.references :friend
       t.timestamps
     end
