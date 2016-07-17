727bbf703babfb51d9527ed02f6b30b33ee12ec1
Kento Watanabe <k3nt0.watanabe@gmail.com>
2016-07-17 16:26:00 +0900

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 NoSessionUser.create!(name:  "Example User",
-                       profile_url: "http://hogehoge.jp")
+                       profile_url: "https://pixabay.com/static/uploads/photo/2013/07/13/13/49/onion-161611_960_720.png")
 
 10.times do |n|
   name  = Faker::Name.name
@@ -8,11 +8,9 @@ NoSessionUser.create!(name:  "Example User",
                profile_url: profile_url)
 end
 
-"""
 @users = NoSessionUser.order(:created_at).take(3)
 20.times do
   message = Faker::Lorem.sentence(5)
   @users.each { |user| NoSessionTalk.create!(message: message,
                                  no_session_user_id: @user.id)}
 end
-"""
