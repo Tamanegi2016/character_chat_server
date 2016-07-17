70cc41813c91f47da88955fdbb7f88cd51f72894
Kento Watanabe <k3nt0.watanabe@gmail.com>
2016-07-17 12:40:29 +0900

    references

--- db/migrate/20160716064823_create_friends.rb
+++ db/migrate/20160716064823_create_friends.rb
@@ -1,8 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[5.0]
   def change
     create_table :friends do |t|
-      t.references :to_user, :no_session_users , index: true
-      t.references :from_user, :no_session_users , index: true
+      t.references :no_session_users, :to_user , index: true
+      t.references :no_session_users, :from_user , index: true
       t.timestamp
     end
   end
